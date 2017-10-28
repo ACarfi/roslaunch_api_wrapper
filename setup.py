b5d43d054cc91cf4dfd91d4a5e4f33d4f4162105
ACarfi <carfi.alessandro@gmail.com>
2017-10-28 15:59:10 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='roslaunch_api_wrapper',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['roslaunch_api_wrapper'],
+      zip_safe=False)
