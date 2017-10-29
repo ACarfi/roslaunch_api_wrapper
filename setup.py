146c1c1e96624bb0da93f5ad28236262c78950bd
ACarfi <carfi.alessandro@gmail.com>
2017-10-29 15:43:15 +0100

    update repository name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='roslaunch_api_wrapper',
+setup(name='roslaunch-api-wrapper',
       version='0.1',
       description='wrapper for rosalunch python api',
-      url='https://github.com/ACarfi/roslaunch_api_wrapper',
-      author='Alessandro Carfì',
+      url='https://github.com/ACarfi/roslaunch-api-wrapper',
+      author='Alessandro Carfi',
       author_email='carfi.alessandro@gmail.com',
       license='MIT',
-      packages=['roslaunch_api_wrapper'],
+      packages=['roslaunch-api-wrapper'],
       zip_safe=False)
