24e86401785460f211e2d5556cc6b56ce867b802
ACarfi <carfi.alessandro@gmail.com>
2017-10-29 17:41:27 +0100

    README type changed MANIFEST added

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='roslaunch-api-wrapper',
-      version='0.1',
+      version='0.2',
       description='wrapper for rosalunch python api',
+      long_description=readme(),
       url='https://github.com/ACarfi/roslaunch-api-wrapper',
       author='Alessandro Carfi',
       author_email='carfi.alessandro@gmail.com',
